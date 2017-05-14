594cbac87ff2bbace626b56e20ab29af0071c4d4
Bob Brazeau <bobbrazeau@gmail.com>
2017-05-14 15:29:23 -0700

    Added extra logging fields
    Now stores the file path of the backup file and stores the total number of DBs checked at the master level

--- Create Sprocs.sql
+++ Create Sprocs.sql
@@ -1,11 +1,77 @@
 USE [loggingDBCC]
 GO
 
+/****** Object:  StoredProcedure [dbo].[dbccLogDB]    Script Date: 5/14/2017 3:28:02 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
+
+-- =============================================
+-- Author:		Bob Brazeau
+-- Create date: 4/15/2017
+-- Description:	save check of each database  
+-- =============================================
+CREATE PROCEDURE [dbo].[dbccLogDB]
+	@masterID int,
+	@dbName nvarchar(255),
+	@filePath nvarchar(255),
+	@errors int,
+	@errorMsg nvarchar(4000),
+	@startTime datetime,
+	@endTime datetime
+AS
+BEGIN
+
+	insert into [dbo].dbcclogDetail (masterID, databaseName, filePath, errors, errMsg, startTime, endTime)
+	values(@masterID, @dbName, @filePath, @errors, @errorMsg, @startTime, @endTime);
+END
+
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[dbccLogEnd]    Script Date: 5/14/2017 3:28:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		Bob Brazeau
+-- Create date: 4/15/2017
+-- Description:	finalize server record summing db's with errors  
+-- =============================================
+CREATE PROCEDURE [dbo].[dbccLogEnd]
+	@id int
+AS
+BEGIN
+	declare @count int = 0, @checked int = 0;
+
+	select @count = sum(errors), @checked = count(*)	
+	from [dbo].dbccLogDetail 
+	where masterID = @id;
+
+	select @count, @checked;
+
+	update [dbo].[dbccLogMaster]
+	set errors = @count, endTime = getdate(), checkedDBs = @checked
+	where id = @id;
+END
+
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[dbccLogStart]    Script Date: 5/14/2017 3:28:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 
 -- =============================================
 -- Author:		Bob Brazeau
@@ -26,56 +92,5 @@ BEGIN
 	RETURN
 END
 
-GO
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
--- =============================================
--- Author:		Bob Brazeau
--- Create date: 4/15/2017
--- Description:	save check of each database  
--- =============================================
-CREATE PROCEDURE [dbo].[dbccLogDB]
-	@masterID int,
-	@dbName nvarchar(255),
-	@errors int,
-	@errorMsg nvarchar(4000),
-	@startTime datetime,
-	@endTime datetime
-AS
-BEGIN
-
-	insert into [dbo].dbcclogDetail (masterID, databaseName, errors, errMsg, startTime, endTime)
-	values(@masterID, @dbName, @errors, @errorMsg, @startTime, @endTime);
-END
-
-GO
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
--- =============================================
--- Author:		Bob Brazeau
--- Create date: 4/15/2017
--- Description:	finalize server record summing db's with errors  
--- =============================================
-CREATE PROCEDURE [dbo].[dbccLogEnd]
-	@id int
-AS
-BEGIN
-	declare @count int = 0;
-	select @count = sum(errors) from [dbo].dbccLogDetail where masterID = @id;
-
-	update [dbo].[dbccLogMaster]
-	set errors = @count, endTime = getdate()
-	where id = @id;
-END
 
 GO
